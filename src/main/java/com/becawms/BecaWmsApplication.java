2882c30346578d0ca04ccc9b9eb1d4fe5d1c92ed
Ricardo de Camargo - PC <ricardocamargoprado@hotmail.com>
2025-12-14 10:08:43 -0300

    Upgrade Java to 21: update java.version

---
+++ src/main/java/com/becawms/BecaWmsApplication.java
@@ -0,0 +1,11 @@
+package com.becawms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecaWmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BecaWmsApplication.class, args);
+    }
+}
